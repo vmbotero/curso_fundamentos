2be9ae19da0c9c7e029d336e633b12b631396114
victor botero <boterogomezvictor@gmail.com>
2021-04-28 22:21:07 -0500

    actualizacion codigo

--- Sensor.java
+++ Sensor.java
@@ -1,102 +1,132 @@
+import java.util.ArrayList;
 public class Sensor{
-private String tipo;
-private double valor;
-public static int tamaño = 8;
-public static int posAñadir = 0;
-public static Sensor[] sensores = new Sensor[8];
+    private String tipo;
+    private double valor;
+    public static int tamaño = 8;
+    public static int posAñadir = 0;     
+    public static Sensor[] sensores = new Sensor[8];
 
- public Sensor(){
-Sensor.sensores[Sensor.posAñadir] = this;
-Sensor.posAñadir = Sensor.posAñadir + 1;
-}
+ 
 
- public Sensor(String t, double v){
-this.tipo = t;
-this.valor = v;
-Sensor.sensores[Sensor.posAñadir] = this;
-Sensor.posAñadir = Sensor.posAñadir + 1;
-}
+    public Sensor(){
+        Sensor.sensores[Sensor.posAñadir] = this;
+        Sensor.posAñadir = Sensor.posAñadir + 1;
+    } 
 
- public String getTipo(){
-return this.tipo;
-}
-public void setTipo(String t){
-this.tipo = t;
-}
+ 
 
- public double getValor(){
-return this.valor;
-}
-public void setValor(double v){
-this.valor = v;
-}
+    public Sensor(String t, double v){
+        this.tipo = t;
+        this.valor = v;
+        Sensor.sensores[Sensor.posAñadir] = this;
+        Sensor.posAñadir = Sensor.posAñadir + 1;
+    }
 
- public String toString(){
-return "Tipo:" + this.getTipo() + "/ Valor:" + this.getValor();
-}
+ 
 
- public static String toStringSensores(){
-String DatosSensores = "";
-for(int i=0; i<Sensor.posAñadir; i++){
-if(i == 0){
-DatosSensores = DatosSensores + ("Sensores:\n" + Sensor.sensores[i].toString());
-}else{
-DatosSensores = DatosSensores + ("\n" + Sensor.sensores[i].toString());
-}
-}
-return DatosSensores;
-}
+    public String getTipo(){
+        return this.tipo;
+    }
 
- public int CantidadSensores(){
-return posAñadir;
-}
+ 
 
- public static String toStringSensoresTemperatura(Sensor[] Arr){
-String DatosSensores = "";
-for(int i=0; i<Sensor.posAñadir; i++){
-if(Arr[i].tipo.equals("temperatura")){
-if(i == 0){
-DatosSensores = DatosSensores + ("Sensores:\n" + Sensor.sensores[i].toString());
-}else{
-DatosSensores = DatosSensores + ("\n" + Sensor.sensores[i].toString());
-}
-}
-}
-return DatosSensores;
-}
+    public void setTipo(String t){
+        this.tipo = t;
+    }
 
- public static String toStringSensoresTemperaturaOrdenados(Sensor[] Arr){
-Sensor[] NuevoArreglo = new Sensor[tamaño];
-for(int i=0; i < Sensor.posAñadir; i++){
-if(Arr[i].tipo.equals("temperatura")){
-NuevoArreglo[i] = Arr[i];
-}else{
-continue;
-}
-}
-int n = NuevoArreglo.length;
-String DatosSensores = "";
-if(n == 0){
-double temp;
-for(int j=1; j<Sensor.posAñadir; j++){
-for(int k=0; k< Sensor.posAñadir-j; k++){
-if(NuevoArreglo[k].valor > NuevoArreglo[k+1].valor){
-temp = NuevoArreglo[k].valor;
-NuevoArreglo[k].valor = NuevoArreglo[k+1].valor;
-NuevoArreglo[k+1].valor = temp;
-}
-}
-}
-for(int i=0; i < Sensor.posAñadir; i++){
-if (i==0){
-DatosSensores = DatosSensores + "Sensores Temperatura Ordenados:\n" + (NuevoArreglo[i]);
-}else{
-DatosSensores = DatosSensores + ("\n" + NuevoArreglo[i]);
-}
-}
-}else{
-DatosSensores = DatosSensores + "No hay sensores tipo temperatura";
-}
-return DatosSensores;
-}
+ 
+
+    public double getValor(){
+        return this.valor;
+    }
+
+ 
+
+    public void setValor(double v){
+        this.valor = v;
+    }
+
+ 
+
+    public String toString(){
+        return "Tipo:" + this.getTipo() + " / Valor:" + this.getValor();
+    }
+
+ 
+
+    public static String toStringSensores(){
+        String DatosSensores = "";
+        for(int i=0; i<Sensor.posAñadir; i++){
+            if(i == 0){
+                DatosSensores = DatosSensores + ("Sensores:\n" + Sensor.sensores[i].toString());
+            }else{
+                DatosSensores = DatosSensores + ("\n" + Sensor.sensores[i].toString());
+            }             
+        }
+        if(DatosSensores.equals("")){
+            DatosSensores = "No se hallaron sensores";
+        }
+        return DatosSensores;
+    }  
+
+ 
+
+    public int CantidadSensores(){
+        return posAñadir;
+    }
+
+ 
+
+    public static String toStringSensoresTemperatura(Sensor[] Arr){
+        String DatosSensores = "";
+        for(int i=0; i<Sensor.posAñadir; i++){
+            if(Arr[i].tipo.equals("temperatura")){
+                if(i == 0){
+                    DatosSensores = DatosSensores + ("Sensores Temperatura:\n" + Sensor.sensores[i].toString());
+                }else{
+                    DatosSensores = DatosSensores + ("\n" + Sensor.sensores[i].toString());
+                }  
+            }
+        }
+        if(DatosSensores.equals("")){
+            DatosSensores = "No se hallaron sensores tipo temperatura";
+        }
+        return DatosSensores;
+    } 
+
+ 
+
+    public static String toStringSensoresTemperaturaOrdenados(Sensor[] Arr){        
+        ArrayList<Sensor> NuevoArreglo = new ArrayList<Sensor>();
+        for(int i=0; i < Sensor.posAñadir; i++){
+            if(Arr[i].tipo.equals("temperatura")){
+                NuevoArreglo.add(Arr[i]);
+            }else{
+                continue;
+            }
+        }        
+        int n = NuevoArreglo.size();       
+        double temp;
+        String DatosSensores = "";
+        for(int j=1; j<n; j++){
+            for(int k=0; k<n-j; k++){
+                if(NuevoArreglo.get(k).valor > NuevoArreglo.get(k+1).valor){
+                    temp = NuevoArreglo.get(k).valor;
+                    NuevoArreglo.get(k).valor = NuevoArreglo.get(k+1).valor;
+                    NuevoArreglo.get(k+1).valor = temp;
+                }
+            }
+        }             
+        for(int i=0; i < n; i++){
+            if (i==0){                
+                DatosSensores = DatosSensores + "Sensores Temperatura Ordenados:\n" + (NuevoArreglo.get(i));                
+            }else{
+                DatosSensores = DatosSensores + ("\n" + NuevoArreglo.get(i));
+            }
+        }
+        if(DatosSensores.equals("")){   
+            DatosSensores = "No se hallaron sensores tipo temperatura para ordenar";
+        }
+        return DatosSensores;
+    }
 }
--- Vehiculo.java
+++ Vehiculo.java
@@ -1,99 +1,117 @@
 public class Vehiculo {
-public static int tamaño=10;
-public static int posAñadir = 0;
-public static Vehiculo[] vehiculos = new Vehiculo[10];
-private int modelo;
-private String marca;
-private double valorComercial;
-private String color;
+    public static Vehiculo[] vehiculos = new Vehiculo[10];    
+    public static int tamaño = 10;
+    public static int posAñadir = 0;    
+    private int modelo;
+    private String marca;
+    private double valorComercial;
+    private String color;    
 
- public Vehiculo(){
-Vehiculo.vehiculos[Vehiculo.posAñadir] = this;
-Vehiculo.posAñadir = Vehiculo.posAñadir + 1;
-}
+ 
 
- public Vehiculo(int mo, String ma,double va){
-this(mo,ma,va,"verde");
-}
+    public Vehiculo(){
+        Vehiculo.vehiculos[Vehiculo.posAñadir] = this;
+        Vehiculo.posAñadir = Vehiculo.posAñadir + 1;
+    }
 
- public Vehiculo(int mo, String ma,double va,String co){
-this.modelo = mo;
-this.marca = ma;
-this.valorComercial = va;
-this.color = co;
-Vehiculo.vehiculos[Vehiculo.posAñadir] = this;
-Vehiculo.posAñadir = Vehiculo.posAñadir + 1;
-}
+ 
 
- public int getModelo(){
-return this.modelo;
-}
+    public Vehiculo(int mo, String ma,double va){
+        this(mo,ma,va,"verde");
+    }
 
- public void setModelo(int mo){
-this.modelo = mo;
-}
+ 
 
- public String getMarca(){
-return this.marca;
-}
+    public Vehiculo(int mo, String ma,double va,String co){
+        this.modelo = mo;
+        this.marca = ma;
+        this.valorComercial = va;
+        this.color = co;
+        Vehiculo.vehiculos[Vehiculo.posAñadir] = this;
+        Vehiculo.posAñadir = Vehiculo.posAñadir + 1;
+    }
 
- public void setMarca(String ma){
-this.marca = ma;
-}
+ 
 
- public double getValorComercial(){
-return this.valorComercial;
-}
+    public int getModelo(){
+        return this.modelo;
+    }
+    public void setModelo(int mo){
+        this.modelo = mo;
+    }
 
- public void setValorComercial(double va){
-this.valorComercial= va;
-}
+ 
 
- public String getColor(){
-return this.color;
-}
+    public String getMarca(){
+        return this.marca;
+    }
+    public void setMarca(String ma){
+        this.marca = ma;
+    }
 
- public void setColor(String co){
-this.color = co;
-}
+ 
 
- public String toString(){
-return "Modelo:" + this.getModelo() + " / Marca:" +
-this.getMarca() + " / Valor Comercial:" +
-this.getValorComercial() + " / Color:" + this.getColor();
-}
+    public double getValorComercial(){
+        return this.valorComercial;
+    }
+    public void setValorComercial(double va){
+        this.valorComercial= va;
+    }
 
- public static String toStringVehiculos(){
-String DatosVehiculos = "";
-for(int i=0; i< Vehiculo.posAñadir; i++){
-if(i == 0){
-DatosVehiculos = DatosVehiculos + ("Vehiculos:\n" + Vehiculo.vehiculos[i].toString());
-}else{
-DatosVehiculos = DatosVehiculos + ("\n" + Vehiculo.vehiculos[i].toString());
-}
-}
-return DatosVehiculos;
-}
+ 
 
- public static int cantidadVehiculos(){
-return posAñadir;
-}
+    public String getColor(){
+        return this.color;
+    }
+    public void setColor(String co){
+        this.color = co;
+    }
 
- public static String toStringVehiculosVerdes(Vehiculo[] Vehi){
-String Datos = "";
-for(int i=0; i< Vehiculo.posAñadir; i++){
-if(Vehi[i].color.equals("verde")){
-if(i == 0){
-Datos = Datos + ("Vehiculos Verdes:\n" + Vehiculo.vehiculos[i].toString());
-}else{
-Datos = Datos + ("\n" + Vehiculo.vehiculos[i].toString());
-}
-}
-}
-if (Datos.equals("")){
-return "No hay vehiculos de color verde";
-}else{
-return Datos;
-}
-}
+ 
+
+    public String toString(){
+        return "Modelo:" + this.getModelo() + " / Marca:" +
+        this.getMarca() + " / Valor Comercial:" + 
+        this.getValorComercial() + " / Color:" + this.getColor();
+    }
+
+ 
+
+    public static String toStringVehiculos(){
+        String DatosVehiculos = "";
+        for(int i=0; i< Vehiculo.posAñadir; i++){
+            if(i == 0){
+                DatosVehiculos = DatosVehiculos + ("Vehiculos:\n"  + Vehiculo.vehiculos[i].toString());
+            }else{
+                DatosVehiculos = DatosVehiculos + ("\n" + Vehiculo.vehiculos[i].toString());
+            }
+        } 
+        return DatosVehiculos; 
+    }
+
+ 
+
+    public static int cantidadVehiculos(){
+        return posAñadir;
+    } 
+
+ 
+
+    public static String toStringVehiculosVerdes(Vehiculo[] Vehi){
+        String Datos = "";       
+        for(int i=0; i< Vehiculo.posAñadir; i++){
+            if(Vehi[i].color.equals("verde")){
+                if(i == 0){
+                    Datos = Datos + ("Vehiculos Verdes:\n" + Vehiculo.vehiculos[i].toString());
+                }else{
+                    Datos = Datos + ("\n" + Vehiculo.vehiculos[i].toString());
+                }
+            }            
+        }   
+        if (Datos.equals("")){
+            return "No hay vehiculos de color verde";
+        }else{
+            return Datos;
+        }
+    }
 }
